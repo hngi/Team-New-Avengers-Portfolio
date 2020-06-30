31389c0e345d7bbd4e33859cd7e68e491263e4f5
Killmoovez <ebirikaugo@gmail.com>
2020-06-30 14:01:43 +0100

    portfolio template-7 files

---
+++ script/main.js
@@ -0,0 +1,9 @@
+function openRightMenu() {
+    document.getElementById("rightMenu").style.display = "block";
+  }
+  
+  function closeRightMenu() {
+    document.getElementById("rightMenu").style.display = "none";
+  }
+  w3.slideshow(".workings", 2000);
+  w3.slideshow(".stories", 3000);
